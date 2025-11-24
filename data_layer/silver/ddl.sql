152df4b0f9822833f40f820cbeb914bbf598901d
Fabio <fabioofonteles1@gmail.com>
2025-11-23 21:54:59 -0300

    fix:correção

--- data_layer/silver/ddl.sql
+++ data_layer/silver/ddl.sql
@@ -1,28 +1,33 @@
 CREATE SCHEMA IF NOT EXISTS silver;
-DROP TABLE IF EXISTS silver.microsoft_security_incident;    
+
+DROP TABLE IF EXISTS silver.microsoft_security_incident;
+
 CREATE TABLE silver.microsoft_security_incident (
-    id BIGINT PRIMARY KEY,
-    org_id INT NOT NULL,
-    incident_id INT NOT NULL,
-    alert_id INT NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
-    detector_id INT NOT NULL,
-    alert_title VARCHAR(500) NOT NULL,
-    category VARCHAR(100) NOT NULL,
-    mitre_techniques VARCHAR(200) NOT NULL,
-    incident_grade INT NOT NULL,
-    entity_type INT NOT NULL,
-    evidence_role INT NOT NULL,
+
+    id BIGINT,
+    org_id BIGINT,
+    incident_id BIGINT,
+    alert_id BIGINT,
+    timestamp TIMESTAMP,
+    detector_id BIGINT,
+    alert_title TEXT,
+    category TEXT,
+    mitre_techniques TEXT,
+    incident_grade TEXT,
+    entity_type TEXT,
+    evidence_role TEXT,
     device_id BIGINT,
-    sha256 CHAR(64),
-    ip_address INET,  
+    sha256 TEXT,
     url TEXT,
-    account_sid VARCHAR(200), 
-    account_upn VARCHAR(255),
-    os_family VARCHAR(50) NOT NULL,
-    os_version VARCHAR(50) NOT NULL,
-    country_code CHAR(2) NOT NULL,
-    state VARCHAR(100) NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    last_verdict VARCHAR(50) NOT NULL
+    account_sid TEXT,
+    account_upn TEXT,
+    os_family TEXT,
+    os_version TEXT,
+    country_code TEXT,
+    state TEXT,
+    city TEXT,
+    last_verdict TEXT
 );
+
+-- incide
+CREATE INDEX IF NOT EXISTS idx_silver_timestamp ON silver.microsoft_security_incident(timestamp);
